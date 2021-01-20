4078565cd4bca02d6869eb6a1561009916940aa7
Aleyna Isnık <aleynaisnik@outlook.com>
2021-01-20 23:51:15 +0300

    20.01.2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArry = items;
+            items = new T[items.Length+1];
+
+            for (int i = 0; i < tempArry.Length; i++)
+            {
+                items[i] = tempArry[i];
+
+            }
+
+            items[item.Length - 1] = item;
+        }
+    }
+    
+}
