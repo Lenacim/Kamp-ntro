14f85c6afd4e0fa83e42a8c3fc71f770788f29b9
Aleyna Isnık <aleynaisnik@outlook.com>
2021-01-24 13:01:50 +0300

    MyDictionary

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Dictionary<string,int> numbers = new Dictionary<string,int>();
+
+            numbers.Add("Aleyna", 123);
+            numbers.Add("Emir", 678);
+            numbers.Add("Büşra", 489);
+            numbers.Add("Emirhan", 124);
+            numbers.Add("Nisa", 564);
+
+            foreach (KeyValuePair<string, int> entry in numbers)
+
+              Console.WriteLine(entry.Key + ":" + entry.Value);
+
+        }
+    }
+}
